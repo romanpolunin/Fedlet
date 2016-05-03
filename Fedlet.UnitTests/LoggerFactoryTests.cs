b69f9b17cadd0566dea0543c4875b49f153ddeb8
Roman Polunin <poluninr@medamerica.com>
2016-05-02 18:40:13 -0700

    switched to NuGet instead of explicitly supplied DLLs, removed usage of Rhino Mocks

--- Fedlet.UnitTests/LoggerFactoryTests.cs
+++ Fedlet.UnitTests/LoggerFactoryTests.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using NUnit.Framework;
-using Rhino.Mocks;
-using SharpTestsEx;
+﻿using NUnit.Framework;
 using Sun.Identity.Common;
 
 namespace Fedlet.UnitTests
@@ -15,23 +9,14 @@ namespace Fedlet.UnitTests
         [Test]
         public void GetLogger_before_SetFactory_should_return_instance_of_FedletLogger()
         {
-            LoggerFactory.GetLogger<LoggerFactoryTests>().Should().Be.InstanceOf<EventLogLogger>();
+            Assert.IsInstanceOf<EventLogLogger>(LoggerFactory.GetLogger<LoggerFactoryTests>());
         }
 
         [Test]
         public void SetFactory_should_replace_the_instance_of_the_static_factory_delegate()
         {
             LoggerFactory.SetFactory(type => null);
-            LoggerFactory.GetLogger<LoggerFactoryTests>().Should().Be.Null();
-        }
-
-        [Test]
-        public void SetFactory_should_set_non_null_logger()
-        {
-            ILogger mockLogger = MockRepository.GenerateMock<ILogger>();
-
-            LoggerFactory.SetFactory(type => mockLogger);
-            LoggerFactory.GetLogger<LoggerFactoryTests>().Should().Be(mockLogger);
+            Assert.IsNull(LoggerFactory.GetLogger<LoggerFactoryTests>());
         }
     }
 }
