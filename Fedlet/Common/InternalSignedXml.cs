dd2a57f3260189dfd4f2c71589041c2241b9a6c4
Andrei Shenets <andrei.shenets@itechart-group.com>
2018-02-05 16:30:47 +0300

    Added additional SignedXml constant

--- Fedlet/Common/InternalSignedXml.cs
+++ Fedlet/Common/InternalSignedXml.cs
@@ -5,6 +5,9 @@
     /// </summary>
     public static class InternalSignedXml
     {
+        /// <summary>Represents the Uniform Resource Identifier (URI) for the standard <see cref="T:System.Security.Cryptography.SHA256" /> digest method for XML digital signatures. This field is constant.</summary>
+        public const string XmlDsigSHA256Url = "http://www.w3.org/2001/04/xmlenc#sha256";
+
         /// <summary>Represents the Uniform Resource Identifier (URI) for the  <see cref="T:System.Security.Cryptography.RSA" /> SHA-256 signature method variation for XML digital signatures. This field is constant.</summary>
         public const string XmlDsigRSASHA256Url = "http://www.w3.org/2001/04/xmldsig-more#rsa-sha256";
 
