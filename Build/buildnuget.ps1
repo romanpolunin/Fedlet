46e1e4282ac90e4694e4b70e52dfe781e0921f1b
Roman Polunin <roman.polunin@outlook.com>
2018-02-26 22:06:31 -0800

    package update

--- Build/buildnuget.ps1
+++ Build/buildnuget.ps1
@@ -5,13 +5,13 @@ Remove-Item -Path .\CHANGES.txt -Force
 Remove-Item -Path .\README -Force
 
 md lib\netstandard2.0
-Copy-Item ..\Fedlet\bin\Release\Fedlet.dll .\lib\netstandard2.0
-Copy-Item ..\Fedlet\bin\Release\Fedlet.xml .\lib\netstandard2.0
-Copy-Item ..\Fedlet\bin\Release\Fedlet.pdb .\lib\netstandard2.0
+Copy-Item ..\Fedlet\bin\Release\netstandard2.0\Fedlet.dll .\lib\netstandard2.0
+Copy-Item ..\Fedlet\bin\Release\netstandard2.0\Fedlet.xml .\lib\netstandard2.0
+Copy-Item ..\Fedlet\bin\Release\netstandard2.0\Fedlet.pdb .\lib\netstandard2.0
 
 Copy-Item ..\NOTICES.txt .\
 Copy-Item ..\LICENSE.txt .\
 Copy-Item ..\CHANGES.txt .\
 Copy-Item ..\README .\
 
-..\..\Nuget\nuget.exe pack Fedlet.2.0.nuspec
+..\..\..\Nuget\nuget.exe pack Fedlet.2.0.nuspec
